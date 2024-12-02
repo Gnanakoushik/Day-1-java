79b494350a5ef3a76d210af533464041b5386b8f
Gnanakoushik <jpranathi04@gmail.com>
2024-12-02 14:42:47 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+    public class B{
+        public static void main(String args[])
+        {
+        int a=12;
+        char b='A';
+        float c=3.23f;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        }
+    }
