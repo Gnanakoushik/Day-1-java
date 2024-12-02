932a7e1d6a672896f8f648deea6cbe888341f4e0
Gnanakoushik <jpranathi04@gmail.com>
2024-12-02 16:23:21 +0530

    Add files via upload

---
+++ E.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class D{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String b=String.format("Congratulations %s you won 5lakhs",a);
+        System.out.println(b);
+    }
+}
