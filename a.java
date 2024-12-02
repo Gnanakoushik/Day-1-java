be9008df557692c735d4917a8f600e30636a1baa
Gnanakoushik <jpranathi04@gmail.com>
2024-12-02 13:48:02 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+public class a{
+    public static void main(String[] args)
+    {
+        PrintWriter pw=new PrintWriter(System.out);
+        pw.println("Hello world");
+        pw.println("This ia Pranathi");
+        pw.println("Statement");
+        pw.close();
+    
+    }
+}
